94d4009b43612e7ea60c6ab931f456d2a5d7d943
Tran, Tho <Tho.Tran@netapp.com>
2023-03-06 17:09:08 -0500

    completed chapter 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 #include <cstdint>
+#include <cstring>
 
 //#define CAPACITY 5000
 #define DEBUG
+
+using namespace std;
 
 auto a  = 8;
 auto b = 12345678901;
@@ -12,30 +15,68 @@ auto d = 3.14;
 auto e = true;
 const auto CAPACITY = 4000;
 
-//using namespace std;
+const int n = 4;
+int age[n];
+float temperature[] = {32.5, 31.4, 5.4, 6.4};
+
+char array_char1[8] = "Hello!";
+char array_char2[] = " How are you doing?";
+string std_str1 = "Love, ";
+string std_str2 = "Tho.";
+
+int int_array[5] = {1, 23, 32, 24, 337};
 
 int main() {
+    float mean = 0;
+    for (int num: int_array) {
+        cout << "Int val in array: " << num << endl;
+        mean += num;
+        cout << "Sum so far: " << mean << endl;
+    };
+    mean/=5;
+
+    cout << "Mean of array elements: " << mean << endl;
+
+
+
+
+    /*
+    strcat(array_char1, array_char2);
+    cout << "char concat result: " << array_char1 << endl;
+    cout << "str concat result: " << std_str1 + std_str2 << endl;
+
+    for (int i=0; i<n; i++){
+        age[i] = i+2;
+    }
+    for (int i=0; i<n; i++) {
+        std::cout << "Age " << i << "th person: " << age[i] << std::endl;
+    }
+    for (float t:temperature) {
+        std::cout << "Temp " << t << std::endl;
+    }
+
     int32_t large = CAPACITY;
     uint8_t small = 37;
 
-#ifdef DEBUG
+    #ifdef DEBUG
     std::cout << "About to perform addition" << std::endl;
-#endif
+    #endif
 
     large += small;
 
     std::cout << "The large number is " << large << std::endl;
 
-//    std::cout << "Hello, World!" << std::endl;
-//    return 0;
-//    std::string str;
-//    std::cout << "Enter your name: ";
-//    std::cin >> str;
-//    std::cout << "Hi " << str << std::endl;
-//    std::cout << typeid(a).name() << std::endl;
-//    std::cout << typeid(b).name() << std::endl;
-//    std::cout << typeid(c).name() << std::endl;
-//    std::cout << typeid(d).name() << std::endl;
-//    std::cout << typeid(e).name() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+    std::string str;
+    std::cout << "Enter your name: ";
+    std::cin >> str;
+    std::cout << "Hi " << str << std::endl;
+    std::cout << typeid(a).name() << std::endl;
+    std::cout << typeid(b).name() << std::endl;
+    std::cout << typeid(c).name() << std::endl;
+    std::cout << typeid(d).name() << std::endl;
+    std::cout << typeid(e).name() << std::endl;
+    */
     return 0;
 }
